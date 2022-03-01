1657e7bf12d6a8f4119498c24ea60f9ddd1a0d6c
Lucas-Masaba <masabaluke@gmail.com>
2022-03-01 03:18:35 +0300

    fix linter errors

--- test.rb
+++ test.rb
@@ -2,24 +2,24 @@ require './person'
 require './student'
 require './teacher'
 
-#person
+# person
 
-person = Person.new(15, false)
+person = Person.new(15, parent_permission: false)
 p person
 p(person.can_use_services?)
 
-person2 = Person.new(20, true)
+person2 = Person.new(20, parent_permission: true)
 p person2
 p(person2.can_use_services?)
 
-#student
+# student
 
-teacher = Student.new(15, 'Bankai', name = 'Ichigo')
+teacher = Student.new(15, 'Bankai', name: 'Ichigo')
 p(teacher)
 p(teacher.can_use_services?)
 
-#teacher
+# teacher
 
-teacher = Teacher.new(40, 'Zampacto', name = 'Zangetsu')
+teacher = Teacher.new(40, 'Zampacto', name: 'Zangetsu')
 p(teacher)
 p(teacher.can_use_services?)
